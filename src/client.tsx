6b7fb47891700f6d2cbe4bbe042cea3ecf109509
Michael Williamson <mike@zwobble.org>
2022-12-11 23:46:50 +0000

    Remove unused imports

--- src/client.tsx
+++ src/client.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { useSimpleSync } from "simple-sync/lib/react";
 
