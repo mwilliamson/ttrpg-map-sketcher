bc7265b49f0c45546fe4642a43cfa2d5afe64747
Michael Williamson <mike@zwobble.org>
2022-12-21 18:07:34 +0000

    Rearrange ToolsView

--- src/ToolsView.tsx
+++ src/ToolsView.tsx
@@ -19,40 +19,41 @@ export default function ToolsView(props: ToolsViewProps) {
   const { onRedo, onUndo, selectedToolType, onSelectToolType, selectedColor, onSelectColor } = props;
 
   return (
-    <div className="flex-container-column justify-content-space-between h-100" style={{width: 100}}>
-      <div>
-        <h2 className="my-md text-center">Tools</h2>
-        <div className="btn-stack m-sm">
-          {allToolTypes.map(toolType => (
-            <button
-              key={toolType.name}
-              className={classNames(
-                "btn btn-primary",
-                selectedToolType === toolType ? "btn-variant-solid" : "btn-variant-outline",
-              )}
-              onClick={() => onSelectToolType(toolType)}
-            >
-              {toolType.name}
-            </button>
-          ))}
-        </div>
-        <div className="m-sm" style={{display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: 5}}>
-          <div style={{height: 40, backgroundColor: selectedColor, border: "1px solid #000", gridColumn: "1 / span 3"}}></div>
-          {fillColors.map(fillColor => (
-            <div
-              key={fillColor}
-              style={{
-                backgroundColor: fillColor,
-                aspectRatio: 1,
-                border: "1px solid #000",
-              }}
-              onClick={() => onSelectColor(fillColor)}
-            >
-            </div>
-          ))}
-        </div>
-      </div>
+    <div className="h-100 overflow-y-auto" style={{width: 100}}>
+      <h2 className="my-md text-center">Tools</h2>
+
       <div className="btn-stack m-sm">
+        {allToolTypes.map(toolType => (
+          <button
+            key={toolType.name}
+            className={classNames(
+              "btn btn-primary",
+              selectedToolType === toolType ? "btn-variant-solid" : "btn-variant-outline",
+            )}
+            onClick={() => onSelectToolType(toolType)}
+          >
+            {toolType.name}
+          </button>
+        ))}
+      </div>
+
+      <div className="m-sm mt-md" style={{display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: 5}}>
+        <div style={{height: 40, backgroundColor: selectedColor, border: "1px solid #000", gridColumn: "1 / span 3"}}></div>
+        {fillColors.map(fillColor => (
+          <div
+            key={fillColor}
+            style={{
+              backgroundColor: fillColor,
+              aspectRatio: 1,
+              border: "1px solid #000",
+            }}
+            onClick={() => onSelectColor(fillColor)}
+          >
+          </div>
+        ))}
+      </div>
+
+      <div className="btn-stack m-sm mt-md">
         <button
           className="btn btn-primary btn-variant-outline"
           disabled={onUndo === null}
